3fc8fb5c7d6136247fe7e5e6f31f6e51f86082d2
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-04-04 19:19:41 +0530

    ..

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from '@/components/homepage/hero';
+import Projects from '@/components/homepage/projects';
 import React from 'react';
 
 const HomePage = () => {
   return (
     <div>
       <HeroSection />
+      <Projects />
     </div>
   );
 };
--- components/homepage/projects.tsx
+++ components/homepage/projects.tsx
@@ -1,3 +1,36 @@
-export default function projects(){
-    return();
+'use client';
+import react from 'react';
+import Link from 'next/link';
+import { Icon } from '@iconify/react';
+
+export default function Projects() {
+  return (
+    <>
+      <div className="h-screen rounded-t-3xl bg-white font-neue-Helvetica text-black">
+        <div className="flex justify-between p-8">
+          <h1 className="mt-12 text-7xl">FEATURED PROJECTS</h1>
+          <div className="mt-12 flex space-x-3">
+            <Link
+              href="https://github.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="rounded-full border border-gray-500 p-2"
+            >
+              <Icon icon="uil:github" className="h-5 w-5" />
+            </Link>
+
+            {/* LinkedIn */}
+            <Link
+              href="https://linkedin.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="rounded-full border border-gray-500 p-2"
+            >
+              <Icon icon="akar-icons:linkedin-fill" className="h-5 w-5" />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
