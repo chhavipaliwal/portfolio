33c06a36fe26f044727bf23ab79cb88a23ef81bd
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-04-06 00:01:42 +0530

    refactor: update button styles and spacing in Projects component for improved layout and accessibility

--- components/homepage/projects.tsx
+++ components/homepage/projects.tsx
@@ -11,16 +11,24 @@ export default function Projects() {
           <div>
             <h1 className="text-[82px]">FEATURED PROJECTS</h1>
           </div>
-          <div className="flex h-fit flex-row">
-            <Link href="https://github.com" className="">
-              <Icon icon="uil:github" className="h-5 w-5" />
+          <div className="flex flex-row space-x-4">
+            <Link
+              href="https://github.com"
+              className="rounded-full border border-gray-500 p-4"
+            >
+              <Icon icon="uil:github" className="h-8 w-8" />
             </Link>
             {/* LinkedIn */}
-            <Link href="https://linkedin.com" className="">
-              <Icon icon="akar-icons:linkedin-fill" className="h-5 w-5" />
+            <Link
+              href="https://linkedin.com"
+              className="rounded-full border border-gray-500 p-4"
+            >
+              <Icon icon="akar-icons:linkedin-fill" className="h-8 w-8" />
             </Link>
           </div>
         </div>
+        <hr className="my-5 border-t border-gray-400" />
+        <div></div>
       </div>
     </>
   );
