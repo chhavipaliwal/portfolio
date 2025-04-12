c4558bd1450a48cef2439406bebcf694f3731ac4
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-04-12 19:57:32 +0530

    feat: update layout in AboutMe and ContactMe components for improved spacing and visual consistency

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
-import { Code } from '@heroui/react';
-import { cn } from '@/lib/utils';
-import HeroSection from '@/components/homepage/hero';
 import HomePage from './homepage/page';
 export default function Home() {
   return (
