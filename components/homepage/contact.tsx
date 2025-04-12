1febe4dbad18ef8f01f2edb111ba191610f66f47
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-04-12 18:34:51 +0530

    feat: update ContactMe component text and layout for improved engagement

--- components/homepage/contact.tsx
+++ components/homepage/contact.tsx
@@ -20,17 +20,23 @@ export default function ContactMe() {
           <div className="flex flex-row justify-between text-white">
             <div className="">
               <h1 className="mr-80 max-w-xl text-[101px] font-bold leading-none tracking-wide">
-                <span className="text-secondary">LET'S</span> BUILD{' '}
-                <span className="text-secondary">SOMETHING</span> AMAZING{' '}
-                <span className="text-secondary">TOGETHER.</span>
+                LET'S CONNECT AND BUILD SOMETHING AMAZING TOGETHER
               </h1>
             </div>
           </div>
-          <div className="text-left">
-            <DiscussText
-              text="LET'S COLLABORATE"
-              className="text-7xl text-primary underline decoration-2 underline-offset-4"
-            />
+          <div>
+            <div className="text-left">
+              <DiscussText
+                text="HELLO@PIXELPAW"
+                className="text-7xl text-primary underline decoration-2 underline-offset-4"
+              />
+            </div>
+            <div className="text-right">
+              <DiscussText
+                text="LET'S COLLABORATE"
+                className="text-7xl text-primary underline decoration-2 underline-offset-4"
+              />
+            </div>
           </div>
         </div>
       </section>
