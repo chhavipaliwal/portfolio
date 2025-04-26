1a6a2818588a2983a149012d4461447209fc669c
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-04-26 23:27:53 +0530

    fix: add missing commas in projects data for improved syntax consistency

--- components/homepage/projects.tsx
+++ components/homepage/projects.tsx
@@ -15,7 +15,7 @@ const skills = [
   'TYPESCRIPT',
   'FRAMER MOTION',
   'GIT & GITHUB',
-  'UI/UX DESIGN'
+  'UI/UX DESIGN',
 ];
 interface ProjectsProps {
   projects: {
@@ -32,27 +32,27 @@ const projectsData: ProjectsProps = {
       title: 'The Policlinic',
       image: '/project/hospital.png',
       link: 'https://example.com/project1',
-      tech: ['NEXT.JS', 'TAILWIND CSS', 'MONGODB']
+      tech: ['NEXT.JS', 'TAILWIND CSS', 'MONGODB'],
     },
     {
       title: 'Invoice Generator',
       image: '/project/invoice.png',
       link: 'https://example.com/project2',
-      tech: ['REACT', 'TYPESCRIPT', 'FRAMER MOTION', 'NODE.JS']
+      tech: ['REACT', 'TYPESCRIPT', 'FRAMER MOTION', 'NODE.JS'],
     },
     {
       title: 'Kittify',
       image: '/project/kittify.png',
       link: 'https://example.com/project3',
-      tech: ['NEXT.JS', 'SHADCN', 'OPENAI API']
+      tech: ['NEXT.JS', 'SHADCN', 'OPENAI API'],
     },
     {
       title: 'Kodi',
       image: '/project/kodi.png',
       link: 'https://example.com/project4',
-      tech: ['HTML', 'CSS', 'JAVASCRIPT']
-    }
-  ]
+      tech: ['HTML', 'CSS', 'JAVASCRIPT'],
+    },
+  ],
 };
 
 export default function Projects() {
@@ -145,5 +145,3 @@ export default function Projects() {
     </>
   );
 }
-
-
