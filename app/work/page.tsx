447cd02cdc6c7b4de1c5eb0cb402cd1ec1525d7a
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-05-11 23:17:04 +0530

    feat(work): enhance project display with animations and restructure layout feat(utility): add utility functions for formatting and date handling refactor(project): remove unused TechBadge component

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,19 +1,74 @@
+'use client';
 import { ProjectGrid } from '@/components/sections/work/project-grid';
 
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-hook-inview';
+
 export default function Home() {
+  const [isOnce] = useState(true);
+
   return (
-    <main className=" translate-y-20 min-h-screen bg-black text-white font-neue-Helvetica-Medium ">
-      <div className="container mx-auto px-4 py-16">
-        <h1 className="text-5xl md:text-7xl mb-4 bg-secondary-500 text-transparent bg-clip-text">
-          My Projects
-        </h1>
-        <p className="text-xl text-gray-400 mb-16 max-w-2xl">
+    <>
+      <div className="flex translate-y-20 px-4 md:px-0 font-extrabold flex-row  mt-8 md:mt-0  items-start md:items-center md:pb-8 text-[60px] leading-[60px] md:text-[110px] md:leading-[110px]">
+        <div className="mt-12 relative">
+          <motion.img
+            src="/sparkle-filled.png"
+            width={70}
+            className="absolute hidden md:block left-[60%] translate-x-[-50%] top-[-45%]"
+            initial="hidden"
+            whileInView="visible"
+            variants={{
+              hidden: { opacity: 0, scale: 2, rotate: 0 },
+              visible: { opacity: 1, scale: 1, rotate: -30 },
+            }}
+            transition={{ duration: 1, type: 'spring', stiffness: 100 }}
+            viewport={{ once: isOnce }}
+          />
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            variants={{
+              hidden: { opacity: 0, y: 50, scale: 0.5 },
+              visible: { opacity: 1, y: 0, scale: 1 },
+            }}
+            transition={{ duration: 1, type: 'spring', stiffness: 100 }}
+            viewport={{ once: isOnce }}
+            className="uppercase"
+          >
+            My
+          </motion.div>
+        </div>
+        <div className="flex relative items-center">
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            variants={{
+              hidden: { opacity: 0, y: 50, scale: 0.5 },
+              visible: { opacity: 1, y: 0, scale: 1 },
+            }}
+            viewport={{ once: isOnce }}
+            transition={{
+              duration: 1,
+              type: 'spring',
+              stiffness: 100,
+              delay: 0.1,
+            }}
+            className="font-extrabold italic font-pp-migra text-secondary"
+          >
+            Projects
+          </motion.div>
+        </div>
+      </div>
+      <div className="mt-20  font-neue-Helvetica-Medium text-2xl maax-w-xl ">
+        <p>
           Explore my latest projects showcasing innovative solutions and
           creative designs
         </p>
-
+      </div>
+      <div className="mt-20">
         <ProjectGrid />
       </div>
-    </main>
+    </>
   );
 }
---
+++ functions/utility.ts
@@ -0,0 +1,58 @@
+export const formatPrice = (price: number) => {
+  return new Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency: 'INR',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(price);
+};
+
+// function to check if the file type is an image or not
+export const isImage = (filename: string) => {
+  if (!filename) return false;
+  const ext = filename.split('.').pop();
+  if (
+    ext === 'jpg' ||
+    ext === 'jpeg' ||
+    ext === 'png' ||
+    ext === 'gif' ||
+    ext === 'webp'
+  ) {
+    return true;
+  }
+  return false;
+};
+
+export const humanReadableDate = (
+  date: string | Date,
+  format: 'full' | 'day-month' = 'full'
+) => {
+  const options: Intl.DateTimeFormatOptions =
+    format === 'day-month'
+      ? { day: 'numeric', month: 'short' }
+      : { day: 'numeric', month: 'short', year: 'numeric' };
+
+  return new Date(date).toLocaleDateString('en-US', options);
+};
+
+export const humanReadableTime = (date: string | Date) => {
+  return new Date(date).toLocaleTimeString('en-US', {
+    hour: 'numeric',
+    minute: '2-digit',
+  });
+};
+
+export function capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export function calculateDays(startDate: string, endDate: string) {
+  const diff = new Date(endDate).getTime() - new Date(startDate).getTime();
+  return Math.ceil(diff / (1000 * 60 * 60 * 24));
+}
+
+export function compareDates(date1: string, date2: string) {
+  if (new Date(date1) > new Date(date2)) return 1;
+  if (new Date(date1) < new Date(date2)) return -1;
+  return 0;
+}
