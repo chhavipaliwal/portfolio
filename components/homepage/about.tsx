59ec1e622ac73a9ad8521300af23b4c38de4392f
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-08-05 00:08:51 +0530

    feat(ai): integrate Gemini AI with API endpoint and hook implementation

--- components/homepage/about.tsx
+++ components/homepage/about.tsx
@@ -1,35 +1,33 @@
 'use client';
 import { Avatar } from '@heroui/react';
 import { SpringElement } from '../ui/spring-element';
+import React from 'react';
 
 export default function AboutMe() {
   return (
     <div
-      className="grid w-full grid-cols-3 overflow-auto rounded-t-[74px] bg-secondary text-background sm:p-8"
+      className="grid w-full grid-cols-3 rounded-t-[74px] bg-secondary p-4 text-background sm:p-8"
       id="about"
     >
-      <h1 className="col-span-1 mt-10 text-6xl text-white">About Me</h1>
-      <div className="col-span-2 mt-10 flex flex-col pb-40 line-clamp-3">
-        <p className="mt-4 text-3xl tracking-wider leading-snug">
-          Hi, I&apos;m <span className="text-primary">Chhavi</span> — a
-          dedicated <span className="text-primary">web developer</span>, and
-          creative thinker who finds joy in writing clean code and designing
-          functional UI. I specialize in framing seamless user experiences using{' '}
-          <span className="text-primary">React</span> &{' '}
-          <span className="text-primary">Tailwind CSS</span>, and love turning
-          ideas into reality with <span className="text-primary">Next.js.</span>{' '}
-          At the heart of my work is a balance of design and logic, fueled by a
-          mindset of continuous learning and attention to detail.
-        </p>
-        <p className="mt-4 text-3xl tracking-wider leading-snug">
-          I&apos;m naturally curious and driven by purpose, always eager to
-          build meaningful projects that connect people and solve{' '}
-          <span className="text-primary">real-world problems.</span> I enjoy
-          working on user-centered design and creating interactive interfaces
-          that leave a lasting impact. Beyond the screen, I&apos;m into music,
-          writing, and dreaming up my next big idea. Let&apos;s collaborate and
+      <h1 className="col-span-full mt-10 text-white lg:col-span-1">about me</h1>
+      <div className="col-span-full mt-10 line-clamp-3 flex flex-col gap-12 pb-40 lg:col-span-2">
+        <Content>
+          &quot;Hi, I&apos;m <span className="text-white">Chhavi</span> — a dedicated{' '}
+          <span className="text-white">web developer</span>, and creative thinker who finds joy in
+          writing clean code and designing functional UI. I specialize in framing seamless user
+          experiences using <span className="text-white">React</span> &{' '}
+          <span className="text-white">Tailwind CSS</span>, and love turning ideas into reality with{' '}
+          <span className="text-white">Next.js.</span> At the heart of my work is a balance of
+          design and logic, fueled by a mindset of continuous learning and attention to detail.
+        </Content>
+        <Content>
+          I&apos;m naturally curious and driven by purpose, always eager to build meaningful
+          projects that connect people and solve{' '}
+          <span className="text-white">real-world problems.</span> I enjoy working on user-centered
+          design and creating interactive interfaces that leave a lasting impact. Beyond the screen,
+          I&apos;m into music, writing, and dreaming up my next big idea. Let&apos;s collaborate and
           build something truly impactful together.
-        </p>
+        </Content>
         <div className="mt-8 flex items-center gap-4">
           <SpringElement>
             <Avatar
@@ -41,14 +39,16 @@ export default function AboutMe() {
               }}
             />
           </SpringElement>
-          <div>
-            <h2 className="text-3xl font-semibold text-foreground">
-              Chhavi Paliwal
-            </h2>
-            <p className="">Web Developer</p>
+          <div className="font-bold">
+            <h2 className="text-3xl font-semibold text-foreground">Chhavi Paliwal</h2>
+            <p>Web Developer</p>
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+const Content = ({ children }: { children: React.ReactNode }) => {
+  return <p className="mt-4 text-xl font-medium leading-snug tracking-[-0.475]">{children}</p>;
+};
