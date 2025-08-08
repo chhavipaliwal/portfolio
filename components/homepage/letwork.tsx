b576e1edc0365e5144c761a4bb402ae281049457
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-08-08 11:19:29 +0530

    style(letswork): remove unnecessary marquee component for cleaner layout

--- components/homepage/letwork.tsx
+++ components/homepage/letwork.tsx
@@ -25,26 +25,6 @@ export default function LetsWork() {
           ))}
         </Marquee>
       </div>
-      <div className="relative overflow-hidden rounded-t-3xl bg-[#A6B8C7] md:rounded-t-large">
-        <Marquee
-          pauseOnHover={false}
-          speed={40}
-          gradient={false}
-          loop={0} // 0 = infinite
-          play={true}
-          direction="left"
-          className="font-neue-Helvetica [--duration:18s]"
-        >
-          {skills.map((skill, index) => (
-            <div
-              key={index}
-              className="mx-2 py-2 pt-8 text-[80px] leading-[100px] text-primary before:mr-2 before:text-secondary before:content-['•'] md:py-10 md:pt-16 md:text-[120px]"
-            >
-              {skill}
-            </div>
-          ))}
-        </Marquee>
-      </div>
     </>
   );
 }
