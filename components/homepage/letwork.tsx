12b3cd490e0b77eaed86ee0ce3b603fbb67ed282
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-08-08 11:19:22 +0530

    style(letswork): enhance layout and styling for improved visual appeal and readability

--- components/homepage/letwork.tsx
+++ components/homepage/letwork.tsx
@@ -5,7 +5,7 @@ const skills = ["LET'S WORK", "LET'S WORK", "LET'S WORK"];
 export default function LetsWork() {
   return (
     <>
-      <div className="relative font-neue-Helvetica -translate-y-72 overflow-hidden rounded-t-[74px] bg-[#A6B8C7] ">
+      <div className="relative -translate-y-72 overflow-hidden rounded-t-3xl bg-[#A6B8C7] font-neue-Helvetica md:rounded-t-large">
         <Marquee
           pauseOnHover={false}
           speed={40}
@@ -18,7 +18,27 @@ export default function LetsWork() {
           {skills.map((skill, index) => (
             <div
               key={index}
-              className="mx-8 mb-14 mt-14 text-[60px] leading-[100px] before:content-['•'] md:text-[120px]"
+              className="mx-2 py-2 pt-8 text-[80px] leading-[80px] before:content-['•'] md:text-[120px]"
+            >
+              {skill}
+            </div>
+          ))}
+        </Marquee>
+      </div>
+      <div className="relative overflow-hidden rounded-t-3xl bg-[#A6B8C7] md:rounded-t-large">
+        <Marquee
+          pauseOnHover={false}
+          speed={40}
+          gradient={false}
+          loop={0} // 0 = infinite
+          play={true}
+          direction="left"
+          className="font-neue-Helvetica [--duration:18s]"
+        >
+          {skills.map((skill, index) => (
+            <div
+              key={index}
+              className="mx-2 py-2 pt-8 text-[80px] leading-[100px] text-primary before:mr-2 before:text-secondary before:content-['•'] md:py-10 md:pt-16 md:text-[120px]"
             >
               {skill}
             </div>
