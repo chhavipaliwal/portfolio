030ef40ac2f80e5a818020d92396534fc0975db3
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2026-01-04 09:51:22 +0530

    Add new image domain to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['chhavi-assets.divinely.dev', 'localhost', 'heroui.pro'],
+    domains: ['chhavi-assets.divinely.dev', 'localhost', 'heroui.pro', 'pub-ab1090e7d272462f9dcdf859626e7687.r2.dev'],
   },
 };
 
